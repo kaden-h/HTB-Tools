0da72eee0d5a816342bdf276e08962324fb8c5c6
kaden-h <kaden.hawley@gmail.com>
2026-04-22 17:17:04 -0400

    added random box script

---
+++ random_box.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+for i in range(1, 35):
+    for j in range(1, 16):
+        print("HTB Machines Retired: Page {} Box {}".format(i, j))
+
+for i in range(1, 10):
+    for j in range(1, 16):
+        print("HTB Sherlock Retired: Page {} Box {}".format(i, j))
+        
+for i in range(1, 44):
+    for j in range(1, 16):
+        print("HTB Challenge Retired: Page {} Box {}".format(i, j))
+        
+print("Pro labs: Mythical (can decline)")
+print("Pro labs: Puppet (can decline)")
+for i in range(1, 7):
+    print("Fortress {} (can decline)".format(i))
+
+print()
+print("Note: You have to keep this updated, this is just to generate for \n wheelofnames.com to make it more fun :)")
+
+print()
+print("#################################################")
+print("### Made with <3 and >:3 by Kaden H           ###")
+print("#################################################")
